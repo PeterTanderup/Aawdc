cf73edbc9280ed1fbd0872590b3e8303869d5bc9
Peter Tanderup <sirgaldev@sirgal.dk>
2016-03-29 09:37:41 +0200

    initial commit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.Models;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactFormViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactFormViewModel model)
+        {
+            // used to send mails
+
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+            //MailMessage message = new MailMessage();
+            //message.To.Add("peter@sirgal.dk");
+            //message.Subject = model.Subject;
+            //message.From = new MailAddress(model.Email, model.Name);
+            //message.Body = model.Message;
+
+            //using (SmtpClient smtp = new SmtpClient())
+            //{
+            //    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //    smtp.UseDefaultCredentials = false;
+            //    smtp.EnableSsl = false;
+            //    smtp.Host = "mail.sirgal.dk";
+            //    smtp.Port = 587;
+            //    smtp.Credentials = new System.Net.NetworkCredential("peter@sirgal.dk", "password");
+            //    smtp.EnableSsl = false;
+            //    // send mail
+            //    smtp.Send(message);
+            //}
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Message");
+
+            comment.SetValue("name", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+
+            Services.ContentService.SaveAndPublishWithStatus(comment);
+
+            TempData["success"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
